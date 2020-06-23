0058ae4645eee52ca72fa3120eee1e1df6504e69
deanchen <antlet2004@gmail.com>
2020-06-23 11:54:43 +0800

    No need to watch every folder, just need to add main.js to the file which wanted to be watched

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,12 @@ module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    contentBase: [
-      path.join(__dirname, "site"),
-      path.join(__dirname, "site/cn"),
-      path.join(__dirname, "site/en"),
-    ],
+    //    contentBase: [
+    //      path.join(__dirname, "site"),
+    //      path.join(__dirname, "site/cn"),
+    //      path.join(__dirname, "site/en"),
+    //    ],
+    contentBase: "site",
     watchContentBase: true,
   },
   entry: "./src/index.js",
