ac67c5b1892300cf4dbb514c10323065513e7c6b
deanchen <antlet2004@gmail.com>
2020-06-23 11:49:45 +0800

    Installed webpack, dev-server; Add main.js, so now can auto update the content; Auto-refresh; USAGE: npm run start  visit: http://localhost:8080/...

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: [
+      path.join(__dirname, "site"),
+      path.join(__dirname, "site/cn"),
+      path.join(__dirname, "site/en"),
+    ],
+    watchContentBase: true,
+  },
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "site"),
+  },
+};
